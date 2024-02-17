55ce6ea83dfe209218e4d85513a29ebea8830283
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-02-17 18:00:12 +0700

    working with styles property to define inline styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      p {
+        color: coral;
+        opacity: 0.5;
+      }
+    `
+  ]
 })
 
 export class ServerComponent {
