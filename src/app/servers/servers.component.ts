d4d9b03e9a1bfb9f422abc52ca53d59385d3c873
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-02-17 17:53:20 +0700

    using template to define inline component instead of using external html

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
