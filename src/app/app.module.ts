a0a3b3dc7f3932675c705629e159375ea0670f64
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-05-18 14:13:56 +0700

    completing basics assignment 1 (create success and warning alert component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
 import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './assignments/basics-assignment-1-start/success-alert/success-alert.component';
+import { WarningAlertComponent } from './assignments/basics-assignment-1-start/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
   ],
   imports: [
     BrowserModule,
