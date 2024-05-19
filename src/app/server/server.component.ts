eded210391662a7fa96aa93f40ce808ba12756c9
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-05-19 18:04:28 +0700

    ngClass directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .offline {
+        font-style: 'italic';
+        color: red;
+      }
+    `
+  ]
 })
 
 export class ServerComponent {
